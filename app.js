8564db8f233da92cc0ddadf384d5632e4604a4f9
Manuel Solomon <bhorsun@yahoo.com>
2021-10-15 14:59:39 -0800

    final code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const pullData = require("./controllers/pullPhonesController.js");
 const getPhones = require("./controllers/getPhones.js");
